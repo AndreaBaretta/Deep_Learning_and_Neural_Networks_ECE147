4cfcb4ca8ec07fdd3a8ed3a3a98ec02c2f7191a1
Andrea Baretta <andrea.m.baretta@gmail.com>
2024-02-09 23:01:57 -0800

    Work on FC net

--- Homework3/nndl/fc_net.py
+++ Homework3/nndl/fc_net.py
@@ -48,11 +48,9 @@ class TwoLayerNet(object):
     # ================================================================ #
 
     self.params['W1'] = weight_scale * np.random.randn(hidden_dims, input_dim)
-#     print(f"{self.params['W1'].shape=}")
     self.params['b1'] = np.zeros(hidden_dims)
     self.params['W2'] = weight_scale * np.random.randn(num_classes, hidden_dims)
     self.params['b2'] = np.zeros(num_classes)
-#     self.params['dropout'] = dropout
 
     # ================================================================ #
     # END YOUR CODE HERE
@@ -88,9 +86,6 @@ class TwoLayerNet(object):
     W1, b1 = self.params['W1'], self.params['b1']
     W2, b2 = self.params['W2'], self.params['b2']
     reg = self.reg
-#     print(f" +++++++++++++++++++++ Things are happening ++++++++++++++++++++++")
-#     print(f"{X.shape=}")
-#     print(f"{W1.shape=}")
     h1, cache_h1 = affine_relu_forward(X, W1, b1)
     z2, cache_z2 = affine_forward(h1, W2, b2)
     scores = z2
@@ -118,7 +113,6 @@ class TwoLayerNet(object):
     #
     #   And be sure to use the layers you prior implemented.
     # ================================================================ #
-#     loss, dL = softmax_loss(np.reshape(X, (len(X),-1)), y)
     loss, dL = softmax_loss(scores, y)
     dh1, dw2, db2 = affine_backward(dL, cache_z2)
     dx, dw1, db1 = affine_relu_backward(dh1, cache_h1)
@@ -160,23 +154,23 @@ class FullyConnectedNet(object):
                weight_scale=1e-2, dtype=np.float32, seed=None):
     """
     Initialize a new FullyConnectedNet.
-    
+
     Inputs:
     - hidden_dims: A list of integers giving the size of each hidden layer.
     - input_dim: An integer giving the size of the input.
     - num_classes: An integer giving the number of classes to classify.
     - dropout: Scalar between 0 and 1 giving dropout strength. If dropout=0 then
-      the network should not use dropout at all.
+    the network should not use dropout at all.
     - use_batchnorm: Whether or not the network should use batch normalization.
     - reg: Scalar giving L2 regularization strength.
     - weight_scale: Scalar giving the standard deviation for random
-      initialization of the weights.
+    initialization of the weights.
     - dtype: A numpy datatype object; all computations will be performed using
-      this datatype. float32 is faster but less accurate, so you should use
-      float64 for numeric gradient checking.
+    this datatype. float32 is faster but less accurate, so you should use
+    float64 for numeric gradient checking.
     - seed: If not None, then pass this random seed to the dropout layers. This
-      will make the dropout layers deteriminstic so we can gradient check the
-      model.
+    will make the dropout layers deteriminstic so we can gradient check the
+    model.
     """
     self.use_batchnorm = use_batchnorm
     self.use_dropout = dropout > 0
@@ -193,22 +187,36 @@ class FullyConnectedNet(object):
     #   biases are initialized to zero and the weights are initialized
     #   so that each parameter has mean 0 and standard deviation weight_scale.
     # ================================================================ #
+
+    for i, dims in enumerate(hidden_dims):
+        if self.use_batchnorm:
+            prev_size = (input_dim if i == 0 else hidden_dims[i-1])
+            self.params[f'gamma_{i+1}'] = np.ones(prev_size)
+            self.params[f'beta_{i+1}'] = np.zeros(prev_size)
+        if i == 0:
+            self.params['W1'] = weight_scale * np.random.randn(hidden_dims[i], input_dim)
+            self.params['b1'] = np.zeros(hidden_dims[i])
+        else:
+            self.params[f'W{i+1}'] = weight_scale * np.random.randn(hidden_dims[i], hidden_dims[i-1])
+            self.params[f'b{i+1}'] = np.zeros(hidden_dims[i])
+    L = len(hidden_dims)+1
+    self.params[f'W{L}'] = weight_scale * np.random.randn(num_classes, hidden_dims[len(hidden_dims)-1])
+    self.params[f'b{L}'] = np.zeros(num_classes)
     
-    pass
 
     # ================================================================ #
     # END YOUR CODE HERE
     # ================================================================ #
-    
+
     # When using dropout we need to pass a dropout_param dictionary to each
     # dropout layer so that the layer knows the dropout probability and the mode
     # (train / test). You can pass the same dropout_param to each dropout layer.
     self.dropout_param = {}
     if self.use_dropout:
-      self.dropout_param = {'mode': 'train', 'p': dropout}
-      if seed is not None:
+        self.dropout_param = {'mode': 'train', 'p': dropout}
+    if seed is not None:
         self.dropout_param['seed'] = seed
-    
+
     # With batch normalization we need to keep track of running means and
     # variances, so we need to pass a special bn_param object to each batch
     # normalization layer. You should pass self.bn_params[0] to the forward pass
@@ -216,11 +224,11 @@ class FullyConnectedNet(object):
     # pass of the second batch normalization layer, etc.
     self.bn_params = []
     if self.use_batchnorm:
-      self.bn_params = [{'mode': 'train'} for i in np.arange(self.num_layers - 1)]
-    
+        self.bn_params = [{'mode': 'train'} for i in np.arange(self.num_layers - 1)]
+
     # Cast all parameters to the correct datatype
     for k, v in self.params.items():
-      self.params[k] = v.astype(dtype)
+        self.params[k] = v.astype(dtype)
 
 
   def loss(self, X, y=None):
@@ -229,34 +237,80 @@ class FullyConnectedNet(object):
 
     Input / output: Same as TwoLayerNet above.
     """
+    """
+    Compute loss and gradient for a minibatch of data.
+
+    Inputs:
+    - X: Array of input data of shape (N, d_1, ..., d_k)
+    - y: Array of labels, of shape (N,). y[i] gives the label for X[i].
+
+    Returns:
+    If y is None, then run a test-time forward pass of the model and return:
+    - scores: Array of shape (N, C) giving classification scores, where
+      scores[i, c] is the classification score for X[i] and class c.
+
+    If y is not None, then run a training-time forward and backward pass and
+    return a tuple of:
+    - loss: Scalar value giving the loss
+    - grads: Dictionary with the same keys as self.params, mapping parameter
+      names to gradients of the loss with respect to those parameters.
+    """  
     X = X.astype(self.dtype)
     mode = 'test' if y is None else 'train'
 
     # Set train/test mode for batchnorm params and dropout param since they
     # behave differently during training and testing.
     if self.dropout_param is not None:
-      self.dropout_param['mode'] = mode   
+        self.dropout_param['mode'] = mode   
     if self.use_batchnorm:
-      for bn_param in self.bn_params:
-        bn_param[mode] = mode
+        for bn_param in self.bn_params:
+            bn_param[mode] = mode
 
     scores = None
-    
+
     # ================================================================ #
     # YOUR CODE HERE:
     #   Implement the forward pass of the FC net and store the output
     #   scores as the variable "scores".
     # ================================================================ #
-
-    pass
+    e = 1e-8
+    reg = self.reg
+    cache = []
+    state = np.reshape(X, (len(X),-1))
+    for i in range(1, self.num_layers):
+        state, cache_ = affine_forward(state, self.params[f'W{i}'], self.params[f'b{i}'])
+        cache.append(cache_)
+        
+        if self.use_batchnorm:
+            mu = state.mean(axis=1)[:,np.newaxis]
+            var = state.var(axis=1)[:,np.newaxis]
+            x_hat = (state - mu)/np.sqrt(var+e)
+            state = self.params[f'gamma_{i}']*x_hat + self.params[f'beta_{i}']
+            cache.append((mu, var))
+        
+        state, cache_ = relu_forward(state)
+        cache.append(cache_)
+        
+        if self.use_dropout:
+            if mode == 'test'
+                state *= (1-self.dropout_param['p'])
+            else:
+                M = np.random.rand((state.shape[1])) < self.dropout_param['p']
+                state *= M
+                cache.append(M)
+    
+    L = self.num_layers
+    state, cache_ = affine_forward(state, self.params[f'W{L}'], self.params[f'b{L}'])
+    cache.append(cache_)
+    scores = state
 
     # ================================================================ #
     # END YOUR CODE HERE
     # ================================================================ #
-    
+
     # If test mode return early
     if mode == 'test':
-      return scores
+        return scores
 
     loss, grads = 0.0, {}
     # ================================================================ #
@@ -266,9 +320,34 @@ class FullyConnectedNet(object):
     #   Be sure your L2 regularization includes a 0.5 factor.
     # ================================================================ #
 
-    pass
+    loss, dL = softmax_loss(scores, y)
+    dL, dw, db = affine_backward(dL, cache_z2)
+    grads[f'W{L}'] = dw
+    grads[f'b{L}'] = db
+    for i in range(self.num_layers-1, 0)
 
     # ================================================================ #
     # END YOUR CODE HERE
     # ================================================================ #
     return loss, grads
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
